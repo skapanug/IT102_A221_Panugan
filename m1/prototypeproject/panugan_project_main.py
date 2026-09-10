a5899d44ee08e783dd97d92ddfc57249baa64150
skapanug <skaPanugan@mcm.edu.ph>
2026-09-10 03:46:18 +0000

    yes2

--- m1/prototypeproject/panugan_project_main.py
+++ m1/prototypeproject/panugan_project_main.py
@@ -9,8 +9,23 @@ wrench.addSale("2026-09-02", 1500)
 
 wrench.addSale("2026-09-03", 1200)
 
+print("Features: Product(Wrench Example) Sales Records - 1, Sales Performance - 2")
+button = int(input())
 
-print("Product:", wrench.getProductName())
+if button == 1:
+        print("Product:", wrench.getProductName())
 
-print("Sales Records:", wrench.getSalesRecords())
- 
+        print("Sales Records:", wrench.getSalesRecords())
+
+        print("Add Sales - 1")
+
+        add = int(input())
+        if add == 1:
+                date = input()
+                sold = int(input())
+                wrench.addSale(date+", ", sold)
+                print("New Sales Records:", wrench.getSalesRecords())
+        else:
+            print()
+else: 
+        print()
